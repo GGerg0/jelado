192b326dcbd35481729ea2b1785bf1ccda7f2276
GGergo <gecsegergo006@gmail.com>
2024-05-03 11:22:39 +0200

    2. feladat

--- jelado.py
+++ jelado.py
@@ -13,3 +13,9 @@ adatok = []
 for sor in f:
     sor = sor.strip().split(" ")
     adatok.append(Jel(sor[0],sor[1],sor[2],sor[3],sor[4],))
+
+sorszam = int(input("Adja meg a jel sorszámát: "))
+if sorszam != 0:
+    sorszam -= 1
+
+print(f"x={adatok[sorszam].x} y={adatok[sorszam].y}")
