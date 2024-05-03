45454a087c282cc193a6c109d902ebfbea42f3a1
GGergo <gecsegergo006@gmail.com>
2024-05-03 13:26:48 +0200

    3. feladat

--- jelado.py
+++ jelado.py
@@ -5,6 +5,11 @@ class Jel:
         self.masodperc=masodperc
         self.x=x
         self.y=y
+    def mp(self):
+        return int((self.ora*3600)+(self.perc*60)+self.masodperc)
+
+def eltelt(ido1,ido2):
+    return int(ido2-ido1)
 
 f = open("jel.txt", "rt")
 
@@ -12,6 +17,8 @@ adatok = []
 
 for sor in f:
     sor = sor.strip().split(" ")
+    for i in range(len(sor)):
+        sor[i] = int(sor[i])
     adatok.append(Jel(sor[0],sor[1],sor[2],sor[3],sor[4],))
 
 sorszam = int(input("Adja meg a jel sorszámát: "))
@@ -19,3 +26,4 @@ if sorszam != 0:
     sorszam -= 1
 
 print(f"x={adatok[sorszam].x} y={adatok[sorszam].y}")
+
